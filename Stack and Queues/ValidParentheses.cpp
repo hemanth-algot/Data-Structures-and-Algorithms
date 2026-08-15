a16e497909604a7c5c037c3d477a65e83c6bce24
Hemanth Algot <algothemanth62@gmail.com>
2026-08-15 03:44:12 +0000

    Add Stack and Queue Solutions

---
+++ Stack and Queues/ValidParentheses.cpp
@@ -0,0 +1,25 @@
+//Problem:Valid Parentheses
+//Platform:LeetCode 20
+//Difficulty:Easy
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('||s[i]=='{'||s[i]=='[')
+              st.push(s[i]);
+            else{
+                if(st.empty()) return false;
+                char ch=st.top();
+                st.pop();
+                if(s[i]==')'&&ch=='('||s[i]=='}'&&ch=='{'||s[i]==']'&&ch=='[')
+                  continue;
+                else
+                 return false;
+            }
+        }
+        return st.empty();
+    }
+};
