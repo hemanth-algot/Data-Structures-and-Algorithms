9ac982a4c60daec4de8c077e1767f616552abe79
Hemanth Algot <algothemanth62@gmail.com>
2026-08-19 02:33:02 +0000

    Add Trapping Rain Water Solution

---
+++ Stack and Queues/TrappingRainWater.cpp
@@ -0,0 +1,34 @@
+//Problem:Trapping Rain Water
+//LeetCode:42
+//Difficulty:Hard
+
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        int l=0,r=n-1,lmax=0,rmax=0,total=0;
+        while(l<r){
+            if(height[l]<=height[r]){
+                if(lmax>height[l]){
+                    total+=lmax-height[l];
+                }
+                else{
+                    lmax=height[l];
+                }
+                l++;
+            }
+            else{
+                if(rmax>height[r]){
+                    total+=rmax-height[r];
+                }
+                else{
+                    rmax=height[r];
+                }
+                 r--;
+            }
+           
+        }
+        return total;
+    }
+};
