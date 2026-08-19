195fa89f239fcfb21fd309c1916586f458e813cd
Hemanth Algot <algothemanth62@gmail.com>
2026-08-19 02:35:33 +0000

    Add Sum Of Subarray Minimums solution

---
+++ Stack and Queues/SumOfSubArrayMinimums.cpp
@@ -0,0 +1,46 @@
+//Problem:Sum Of Subarray Minimums
+//LeetCode:907
+//Difficulty:Medium
+
+class Solution {
+public:
+    vector<int> findnse(vector<int>&arr){
+        int n=arr.size();
+        vector<int>nse(n);
+        stack<int>st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty()&&arr[st.top()]>=arr[i]){
+                 st.pop();
+            }
+            nse[i]=st.empty()?n:st.top();
+            st.push(i);
+        }
+     return nse;
+    }
+    vector<int> findpse(vector<int>&arr){
+        int n=arr.size();
+        vector<int>pse(n);
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            while(!st.empty()&&arr[st.top()]>arr[i]){
+                 st.pop();
+            }
+            pse[i]=st.empty()?-1:st.top();
+            st.push(i);
+        }
+     return pse;
+    }
+    int sumSubarrayMins(vector<int>& arr) {
+        int n=arr.size();
+        vector<int>nse=findnse(arr);
+        vector<int>pse=findpse(arr);
+        long long total=0;
+        int mod=(int)(1e9+7);
+        for(int i=0;i<n;i++){
+           int left=i-pse[i];
+           int right=nse[i]-i;
+           total=(total+(right*left*1LL*arr[i])%mod)%mod;
+        }
+        return total;
+    }
+};
