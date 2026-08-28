59d2688ffbf49a6013a273b1d15119959481f402
Hemanth Algot <algothemanth62@gmail.com>
2026-08-28 03:43:48 +0000

    Add Check Adjacent Digit Differences

---
+++ Strings/CheckAdjacentDigitDifferences.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAdjacentDiffAtMostTwo(string s) {
+        int n=s.size();
+        for(int i=0;i<n-1;i++){
+            int a=s[i]-'0';
+            int b=s[i+1]-'0';
+            int value=abs(a-b);
+            if(value>2)
+              return false;
+        }
+        return true;
+    }
+};
